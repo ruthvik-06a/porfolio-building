b1acf085786590dceaea6f118b457f3fac3767e9
dhanyashree.k.s <dhanyashreesreenivasa@gmail.com>
2026-09-07 12:12:56 +0530

    Update main.c

--- line_editor/main.c
+++ line_editor/main.c
@@ -8,6 +8,7 @@ char lines[MAX_LINES][MAX_LENGTH];
 int lineCount = 0;
 
 
+/* INSERT A LINE */
 void insertLine()
 {
     int position;
@@ -20,7 +21,15 @@ void insertLine()
     }
 
     printf("Enter line number (1 to %d): ", lineCount + 1);
-    scanf("%d", &position);
+
+    if (scanf("%d", &position) != 1)
+    {
+        printf("Invalid line number! Please enter a number.\n");
+
+        while (getchar() != '\n');
+
+        return;
+    }
 
     if (position < 1 || position > lineCount + 1)
     {
@@ -35,13 +44,13 @@ void insertLine()
 
     text[strcspn(text, "\n")] = '\0';
 
-    // Shift lines DOWN
+    /* Shift lines DOWN */
     for (int i = lineCount; i >= position; i--)
     {
         strcpy(lines[i], lines[i - 1]);
     }
 
-    // Insert the new text
+    /* Insert new line */
     strcpy(lines[position - 1], text);
 
     lineCount++;
@@ -50,6 +59,7 @@ void insertLine()
 }
 
 
+/* DELETE A LINE */
 void deleteLine()
 {
     int position;
@@ -61,7 +71,15 @@ void deleteLine()
     }
 
     printf("Enter line number to delete (1 to %d): ", lineCount);
-    scanf("%d", &position);
+
+    if (scanf("%d", &position) != 1)
+    {
+        printf("Invalid line number! Please enter a number.\n");
+
+        while (getchar() != '\n');
+
+        return;
+    }
 
     if (position < 1 || position > lineCount)
     {
@@ -69,7 +87,7 @@ void deleteLine()
         return;
     }
 
-    // Shift lines UP
+    /* Shift lines UP */
     for (int i = position - 1; i < lineCount - 1; i++)
     {
         strcpy(lines[i], lines[i + 1]);
@@ -81,6 +99,7 @@ void deleteLine()
 }
 
 
+/* DISPLAY DOCUMENT */
 void displayDocument()
 {
     if (lineCount == 0)
@@ -100,41 +119,114 @@ void displayDocument()
 }
 
 
+/* SEARCH TEXT */
+void searchText()
+{
+    char word[MAX_LENGTH];
+    int found = 0;
+
+    getchar();
+
+    printf("Enter word or phrase to search: ");
+    fgets(word, MAX_LENGTH, stdin);
+
+    word[strcspn(word, "\n")] = '\0';
+
+    for (int i = 0; i < lineCount; i++)
+    {
+        if (strstr(lines[i], word) != NULL)
+        {
+            printf("Found in line %d: %s\n", i + 1, lines[i]);
+            found = 1;
+        }
+    }
+
+    if (!found)
+    {
+        printf("Word or phrase not found.\n");
+    }
+}
+
+
+/* COUNT LINES AND WORDS */
+void countWords()
+{
+    int totalWords = 0;
+
+    for (int i = 0; i < lineCount; i++)
+    {
+        char temp[MAX_LENGTH];
+
+        strcpy(temp, lines[i]);
+
+        char *word = strtok(temp, " \t\n");
+
+        while (word != NULL)
+        {
+            totalWords++;
+            word = strtok(NULL, " \t\n");
+        }
+    }
+
+    printf("Number of lines: %d\n", lineCount);
+    printf("Number of words: %d\n", totalWords);
+}
+
+
+/* MAIN PROGRAM */
 int main()
 {
-    int choice;
+    char command[20];
+
+    printf("===== LINE EDITOR =====\n");
+    printf("Type 'help' to see available commands.\n");
 
     while (1)
     {
-        printf("\n===== LINE EDITOR =====\n");
-        printf("1. Insert line\n");
-        printf("2. Delete line\n");
-        printf("3. Display document\n");
-        printf("4. Exit\n");
+        printf("\n> ");
 
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+        scanf("%19s", command);
 
-        switch (choice)
+        if (strcmp(command, "insert") == 0)
         {
-            case 1:
-                insertLine();
-                break;
-
-            case 2:
-                deleteLine();
-                break;
-
-            case 3:
-                displayDocument();
-                break;
-
-            case 4:
-                printf("Exiting Line Editor...\n");
-                return 0;
-
-            default:
-                printf("Invalid choice! Try again.\n");
+            insertLine();
+        }
+        else if (strcmp(command, "delete") == 0)
+        {
+            deleteLine();
+        }
+        else if (strcmp(command, "display") == 0)
+        {
+            displayDocument();
+        }
+        else if (strcmp(command, "search") == 0)
+        {
+            searchText();
+        }
+        else if (strcmp(command, "count") == 0)
+        {
+            countWords();
+        }
+        else if (strcmp(command, "help") == 0)
+        {
+            printf("\nAvailable commands:\n");
+            printf("insert  - Insert a new line\n");
+            printf("delete  - Delete a line\n");
+            printf("display - Display the document\n");
+            printf("search  - Search for a word or phrase\n");
+            printf("count   - Count lines and words\n");
+            printf("quit    - Exit the editor\n");
+        }
+        else if (strcmp(command, "quit") == 0)
+        {
+            printf("Exiting Line Editor...\n");
+            break;
+        }
+        else
+        {
+            printf("Unknown command! Type 'help' for available commands.\n");
         }
     }
+
+    return 0;
 }
